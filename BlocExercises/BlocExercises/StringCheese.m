4dba729ef7f8798141dc056ff2e18461edda445c
Jonathan Hedstrom <delnorte91@gmail.com>
2016-05-26 14:12:54 -0500

    Make changes to StringCheese.m after consulting with mentor

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,69 +12,36 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    
-    NSString *favoriteCheeseStart = @"My favorite cheese is tempCheese.";
-    
-    NSString *favoriteCheeseSaying = [favoriteCheeseStart stringByReplacingOccurrencesOfString:@"tempCheese" withString:cheeseName];
-    
-//    NSLog(@"%@", favoriteCheeseSaying);  // Debug
-    
-//    Also considered this option to prevent having to create intermediate string...
-//    favoriteCheeseSaying = [NSString stringWithFormat:@"%@%@%@", @"My favorite cheese is ", cheeseName, @"."];
-    
-//    NSLog(@"%@", favoriteCheeseSaying);  // Debug
+ 
+    NSString *favoriteCheeseSaying = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
     
     return favoriteCheeseSaying;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     
-    NSMutableString *nameNoCheese = [NSMutableString stringWithString:cheeseName];
-    
-    if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
-        /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
-        
-        // get range of cheese word
-        NSRange cheeseRange = [cheeseName rangeOfString:@"cheese" options:NSCaseInsensitiveSearch];
-        
-        // modify range to account for leading space
-        cheeseRange.location = (cheeseRange.location - 1);
-        cheeseRange.length = (cheeseRange.length + 1);
-        
-        // modify string to remove cheese characters
-        [nameNoCheese deleteCharactersInRange:cheeseRange];
-        
-    } else {
-        /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
-        // Nothing to do here..
-    }
-
-    /*
-     (You will learn more about if/else statements in a later checkpoint.)
-     */
+    NSString *nameNoCheese = [cheeseName stringByReplacingOccurrencesOfString:@" cheese" withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, cheeseName.length)];
     
     return nameNoCheese;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     
-    NSString *numberOfCheeses;
-    
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
         
-        numberOfCheeses = @"1 cheese";
+        NSString *numberOfCheeses = @"1 cheese";
+        return numberOfCheeses;
         
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
-        numberOfCheeses = [NSString stringWithFormat:@"%lu%@", (unsigned long)cheeseCount, @" cheeses"];
+        NSString *numberOfCheeses = [NSString stringWithFormat:@"%lu cheeses", (unsigned long)cheeseCount];
+        return numberOfCheeses;
     }
     
     /*
      (You will learn more about if/else statements in a later checkpoint.)
      */
-    
-    return numberOfCheeses;
 }
 
 @end
