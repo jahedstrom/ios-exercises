25a62d3fe45a69e1fc587e9ef61a90886c58514e
Jonathan Hedstrom <delnorte91@gmail.com>
2016-05-28 14:01:23 -0500

    Assignment 12 completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,30 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+   
+    NSMutableString *intString = [[NSMutableString alloc] init];
+   
+    NSInteger lowNumber, highNumber;
+    
+    if (number <= otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    
+ // In my opinion, this is more messy to read that just creating two local variables for the for loop..
+    
+ // for (NSInteger n = (number <= otherNumber ? number : otherNumber) ; n <= (number <= otherNumber ? otherNumber : number); n++) {
+    
+    for (NSInteger n = lowNumber; n <= highNumber; n++) {
+        [intString appendFormat:@"%ld", (long) n];
+    }
+    
+    
+    return intString;
 }
 
 @end
