fe1c616eba46c013df913b5e72c5546a8779bce2
Jonathan Hedstrom <delnorte91@gmail.com>
2016-06-01 00:23:45 -0500

    Assignment 15 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,53 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    
+    // We must know the type of number in NSNumber for this to be accurate
+    NSNumber *twiceNum = @([number intValue] * 2);
+    
+    
+    return twiceNum;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSInteger lowNumber = number;
+    NSInteger highNumber = otherNumber;
+    
+    NSMutableArray *numArray = [[NSMutableArray alloc] initWithCapacity:1];
+    
+    if (lowNumber > highNumber) {
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    
+    // can't use loop index to assign array elements..
+    int i = 0;
+    
+    for (NSInteger n = lowNumber; n <= highNumber; n++) {
+        numArray[i] = [NSNumber numberWithInteger:n];
+        i++;
+    }
+    
+    return numArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    NSInteger lowNumber = [arrayOfNumbers[0] integerValue];
+    
+    // Start loop index at 1 since we already have the first element
+    for (NSUInteger i = 1; i < arrayOfNumbers.count; i++ ) {
+        NSInteger lowTest = [arrayOfNumbers[i] integerValue];
+        if (lowTest < lowNumber) {
+            lowNumber = lowTest;
+        }
+    }
+    
+    
+    return lowNumber;
 }
 
 @end
