bf08a9fa3f14c8d365419798608f70cc5b7eeca4
Jonathan Hedstrom <delnorte91@gmail.com>
2016-06-07 14:24:59 -0500

    Update playground with changes discussed

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Foundation
 
 /*
 
@@ -9,7 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return "My favorite cheese is " + cheese
+    return "My favorite cheese is " + cheese + "."
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -30,10 +29,13 @@ numberArray.append(5)
 var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
-if numberDictionary.updateValue("five", forKey: 5) == nil {
-    numberDictionary
-}
+//if numberDictionary.updateValue("five", forKey: 5) == nil {
+//    numberDictionary
+//}
 
+numberDictionary[5] = "five"
+
+numberDictionary
 
 /*
 
@@ -66,31 +68,31 @@ let picard = [
     "information": "Captain of the USS Enterprise",
     "favorite drink": "tea, Earl Grey, hot"]
 
-let characters = [worf, picard]
+let mentor = [
+    "name": "Matthew Mentor",
+    "rank": "captain",
+    "information": "iOS guru",
+    "favorite drink": "tea, Earl Grey, hot"]
+
+let characters = [worf, picard, mentor]
 
 func favoriteDrinksArrayForCharacters(characters:[[String : String]]) -> [String] {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
     var drinks = [String]()
-    for name in characters {
-        if let drinkType = name["favorite drink"] {
+    for character in characters {
+        if let drinkType = character["favorite drink"] {
+            // check for dupes..
+            if !drinks.contains(drinkType) {
             drinks.append(drinkType)
+            }
         }
             }
     return drinks
 }
-    // above seems better than this...
-/*
-    for name in characters {
-        drinks.append(name["favorite drink"]!)
-    }
-    return drinks
-}
-*/
-    
+
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
 
-favoriteDrinks
 
 /*
 
@@ -133,22 +135,22 @@ let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
 func arrayToString(inputArray : [String]) -> String {
-    var output = ""
-    var i = 1
-    for item in inputArray {
-        if i < inputArray.count {
-            output += item + ";"
-            i += 1
-        } else {
-            output += item
-        }
-    }
+//    var output = ""
+//    var i = 1
+//    for item in inputArray {
+//        if i < inputArray.count {
+//            output += item + ";"
+//            i += 1
+//        } else {
+//            output += item
+//        }
+//    }
 //    another way to do it... skip the if/else
 //    output.removeAtIndex(output.endIndex.predecessor())
     
 //     an even simpler way..
-//    let c = strings.joinWithSeparator(";")
-    
+    let output = strings.joinWithSeparator(";")
+
     return output
 }
 
@@ -173,7 +175,7 @@ var cerealSorted = cerealArray.sort( { (first, second ) -> Bool in
 
 // or using operator overloading..
 
-var cerealSortedOverload = cerealArray.sort({a, b in a < b})
+var cerealSortedOverload = cerealArray.sort(<)
 cerealSortedOverload
 
 cerealSorted == cerealSortedOverload
